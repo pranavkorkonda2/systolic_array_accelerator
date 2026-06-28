a810f8f5ea6b6f2aa40a5e3d0cb9d61dd18069d0
Pranav Korkonda <pranavkorkonda2@gmail.com>
2026-06-28 06:54:39 -0700

    feat(rtl): implement parameterized skew_buffer for systolic delay lines
    - Design a reusable, parameterized shift register line module
    
    - Implement a compile-time bypass option when DEPTH parameter is 0
    
    - Add synchronous reset mechanism to clear internal pipeline registers
    
    - Utilize non-blocking assignments to ensure accurate cycle-by-cycle data rippling
    
    - Provide continuous output assignment from the final stage of the register chain

---
+++ systolic_array_accelerator.srcs/sources_1/new/skewer_buffer.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/16/2026 10:55:45 PM
+// Design Name: 
+// Module Name: skewer_buffer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module skewer_buffer #(
+parameter DATA_W = 8,
+parameter DEPTH = 0)(
+input wire CLK,
+input wire RST,
+input wire [DATA_W-1:0] data_in,
+output wire [DATA_W-1:0] data_out);
+
+if (DEPTH == 0) begin: bypass
+    assign data_out = data_in;
+    // Literally, if the depth is 0, then this module will just be a 
+    // Bypass wire. However, if depth > 0, then a true shift register chain
+    // will be instantiated
+end else begin: shift_reg
+    reg [DATA_W-1:0] shift_pipeline [0:DEPTH-1];
+    integer i;
+    
+    always @(posedge CLK) begin
+    if (RST) begin
+        for (i = 0; i < DEPTH; i = i + 1) begin
+            shift_pipeline[i] <= 0;
+            // cache cleared from pipelines
+        end
+    end else begin
+        shift_pipeline[0] <= data_in;
+        for (i = 1; i < DEPTH; i = i + 1) begin
+            shift_pipeline[i] <= shift_pipeline[i - 1];
+            // shifting data forward by one position on every clock cycle.
+            // on rising edge of CLK, whatever value is currently sitting 
+            // on the external data_in pin is captured and saved into 
+            // the very first flip-flop stage: shift_pipeline[0].
+        end
+    end
+end
+assign data_out = shift_pipeline[DEPTH-1];
+end
+endmodule
+    
+
+
+endmodule
